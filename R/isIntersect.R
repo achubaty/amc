f6b53aa7b41dcf5ddfaa5c5b99849af93ef92ee1
Alex Chubaty <alex.chubaty@gmail.com>
2017-04-11 08:46:41 -0600

    cleanup and fix errors
    @MelinaHoule, please see comments provided in code review

--- R/isIntersect.R
+++ R/isIntersect.R
@@ -1,42 +1,44 @@
-##################################################################################
-#' Do SPatial objects intersect
+################################################################################
+#' Do spatial objects intersect?
 #'
-#' is Intersect verifies if the extent of two Spatial objects are intersecting. Returns TRUE if its argument intersect.
-#' This function was created to avoid empty output created mask() function in dataPrepRaster. Mask need the layers
+#' Verifies whether the extent of two spatial objects intersect.
 #'
-#' @param x   A \code{RasterLayer} object or the name of one.
-#' @param layer A RasterLayer object or a SpatialPolygonDataFrame object.
+#' @param x     A \code{RasterLayer} object or the name of one.
+#' @param layer A \code{RasterLayer} object or a \code{SpatialPolygonDataFrame} object.
 #'
-#' @return Logical TRUE if the intersect does occur between the 2 layers.
+#' @return Logical. \code{TRUE} if the intersect does occur between the layers.
 #'
-#' @importFrom raster extent crop raster
 #' @docType methods
+#' @export
+#' @importFrom raster crop extent raster
 #'
 #' @author Melina Houle
 #' @examples
-#' library(raster)
-#' library(maptools)
-#' data("wrld_simpl")
+#' if (require(maptools)) {
+#'   library(raster)
+#'   library(maptools)
+#'   data("wrld_simpl")
 #'
-#' # Set
-#' outputPath <- tempdir()
+#'   # From scratch, random raster
+#'   xy <- matrix(rnorm(2500), 50, 50)
+#'   inputrast <- raster(xy)
 #'
-#' #From scratch, random raster
-#' xy <- matrix(rnorm(2500),50,50)
-#' inputrast <- raster(xy)
+#'   # Set Canada extent and projection
+#'   extent(inputrast) <- c(-170, -15, 25, 100)
+#'   projection(inputrast) <- CRS("+proj=longlat +datum=WGS84")
 #'
-#' #Set Canada extent and projection
-#' extent(inputrast) <- c(-170,-15,25,100)
-#' projection(inputrast) <- CRS("+proj=longlat +datum=WGS84")
+#'   # Extract Canada boundary from wrld_simpl
+#'   canada <- wrld_simpl[which(wrld_simpl@data$NAME == "Canada"),]
 #'
-#' #Extract Canada boundary from wrld_simpl
-#' canada<-wrld_simpl[which(wrld_simpl@data$NAME =='Canada'),]
-#'
-#' isIntersect(inputrast,canada)
-isIntersect<-function(x, layer){
-  if (is(layer,"SpatialPolygonsDataFrame")) lExtent <-extent(raster(layer))
-  if (is(layer,"RasterLayer")) lExtent <-extent(layer)
-  i<-tryCatch(!is.null(crop(x,lExtent)), error=function(e) return(FALSE))
-  if(!i)  message("Warning: The two layers do not overlap")
+#'   isIntersect(inputrast, canada)
+#' }
+isIntersect <- function(x, layer) {
+  if (is(layer,"SpatialPolygonsDataFrame")) {
+    lExtent <- extent(raster(layer))
+  } else if (is(layer,"RasterLayer")) {
+    lExtent <- extent(layer)
+  }
+  i <- tryCatch(!is.null(crop(x, lExtent)), error = function(e) return(FALSE))
+  if (!i) message("Warning: The two layers do not overlap")
   return(i)
 }
--- tests/testthat/test-dataPrepRast.R
+++ tests/testthat/test-dataPrepRast.R
@@ -1,4 +1,4 @@
-test_that("test-dataPrepRast", {
+test_that("dataPrepRast produces correct output", {
   library(raster)
   library(maptools)
   library(sp)
@@ -11,86 +11,88 @@ test_that("test-dataPrepRast", {
 
   data("wrld_simpl")
 
-  #From scratch, random raster
-  xy <- matrix(rnorm(2500),50,50)
+  # From scratch, random raster
+  xy <- matrix(rnorm(2500), 50, 50)
   inputrast <- raster(xy)
-  #Set input raster as USA-Canada extent and set projection
-  extent(inputrast) <- c(-170,-15,35,100)
-  projection(inputrast) <- CRS("+proj=longlat +datum=WGS84")
-  #Extract Canada boundary from wrld_simpl
-  canada<-wrld_simpl[which(wrld_simpl@data$NAME =='Canada'),]
 
-  #dataPrepRast
-  x<-dataPrepRast(x=inputrast, cropWith=canada, maskWith= canada,
-                  reprojectTo= inputrast, timings = TRUE)
+  # Set input raster as USA-Canada extent and set projection
+  extent(inputrast) <- c(-170, -15, 35, 100)
+  projection(inputrast) <- CRS("+proj=longlat +datum=WGS84")
+
+  # Extract Canada boundary from wrld_simpl
+  canada <- wrld_simpl[which(wrld_simpl@data$NAME == "Canada"),]
+
+  # dataPrepRast
+  x <- dataPrepRast(inputrast, cropWith = canada, maskWith = canada,
+                    reprojectTo = inputrast, timings = FALSE)
 
   # Expect output class to be RasterLayer
-  expect_true(is(x,'RasterLayer'))
+  expect_true(is(x, "RasterLayer"))
 
   ## Point to test output raster
   # Create point in USA
-  coordsUSA <-data.frame(x=-100, y=40)
-  ptUSA<-SpatialPoints(coordsUSA, proj4string=CRS(projection(x)))
-  ## Create point in Hudson Bay
-  coordsHudsonBay <-data.frame(x=-85, y=60)
-  ptHB<-SpatialPoints(coordsHudsonBay, proj4string=CRS(projection(x)))
-  ## Create point in Canada (Northwest Territory)
-  coordsNWT <-data.frame(x=-100, y=65)
-  ptNWT<-SpatialPoints(coordsNWT, proj4string=CRS(projection(x)))
+  coordsUSA <- data.frame(x = -100, y = 40)
+  ptUSA <- SpatialPoints(coordsUSA, proj4string = CRS(projection(x)))
+  # Create point in Hudson Bay
+  coordsHudsonBay <- data.frame(x = -85, y = 60)
+  ptHB <- SpatialPoints(coordsHudsonBay, proj4string = CRS(projection(x)))
+  # Create point in Northwest Territories
+  coordsNWT <- data.frame(x = -100, y = 65)
+  ptNWT <- SpatialPoints(coordsNWT, proj4string = CRS(projection(x)))
 
   ##TEST CROP
-  x<-dataPrepRast(x=inputrast, cropWith=canada, maskWith= NULL,
-                  reprojectTo= inputrast,  timings = TRUE)
+  x <- dataPrepRast(inputrast, cropWith = canada, maskWith = NULL,
+                    reprojectTo = inputrast,  timings = FALSE)
 
   # Expect USA point to intersect data on input raster (USA-Canada)
-  valueUSA<-extract(inputrast,ptUSA)
+  valueUSA <- extract(inputrast,ptUSA)
   expect_false(is.na(valueUSA))
+
   # Expect USA point to be NA on output raster (Canada extent)
-  valueUSA2<-extract(x,ptUSA)
+  valueUSA2 <- extract(x, ptUSA)
   expect_true(is.na(valueUSA2))
 
   # Expect Canada point to intersect data on output raster (crop = Canada, mask = NULL)
-  valueNWT<-extract(x,ptNWT)
+  valueNWT <- extract(x, ptNWT)
   expect_false(is.na(valueNWT))
 
   # Expect Hudson Bay point to intersect data on output raster (crop = Canada, mask = NULL)
-  valueHB<-extract(x,ptHB)
+  valueHB <- extract(x, ptHB)
   expect_false(is.na(valueHB))
 
   ## TEST MASK
-  x<-dataPrepRast(x=inputrast, cropWith=canada, maskWith= canada,
-                  reprojectTo= inputrast,  timings = TRUE)
+  x <- dataPrepRast(inputrast, cropWith = canada, maskWith = canada,
+                    reprojectTo = inputrast,  timings = FALSE)
 
   # Expect Hudson Bay point to be NA on output raster (mask = Canada)
-  valueHB<-extract(x,ptHB)
+  valueHB <- extract(x, ptHB)
   expect_true(is.na(valueHB))
-
 
   ## TEST REPROJECT
   # Change canada projection before running dataPrepRast
-  canadaTransform<-spTransform(canada, CRS("+init=epsg:3857"))
+  canadaTransform <- spTransform(canada, CRS("+init=epsg:3857"))
 
   # Expect projection to be different
-  expect_false(compareCRS(x,canadaTransform))
+  expect_false(compareCRS(x, canadaTransform))
 
   # Expect error because input raster and CanadaTransform do not overlap due to spTransform
   expect_error(crop(x, canadaTransform))
 
   # Run dataPrepRast using input arguments with different projection
-  x<-dataPrepRast(x=inputrast, cropWith=canada, maskWith= canada,
-                  reprojectTo= inputrast,  timings = TRUE)
-  y<-dataPrepRast(x=inputrast, cropWith=canadaTransform, maskWith= canadaTransform,
-                  reprojectTo= inputrast,  timings = TRUE)
+  x <- dataPrepRast(inputrast, cropWith = canada, maskWith = canada,
+                    reprojectTo = inputrast,  timings = FALSE)
+  y <- dataPrepRast(inputrast, cropWith = canadaTransform, maskWith = canadaTransform,
+                    reprojectTo = inputrast,  timings = FALSE)
 
   # Expect x to be identical to y because dataPrepRast reproject data before processing
   expect_that(x, is_identical_to(y))
 
   ## TEST ERROR WHEN CROP/MASK DO NOT OVERLAP
   # Extract Mexico boundary to test non-overlapping crop and mask
-  mexico<-wrld_simpl[which(wrld_simpl@data$NAME =='Mexico'),]
+  mexico <- wrld_simpl[which(wrld_simpl@data$NAME == "Mexico"),]
 
   # Expect error if crop/mask layer do not overlap input raster
-  expect_error(dataPrepRast(x=inputrast, cropWith=mexico, maskWith= mexico,
-                          reprojectTo= inputrast, timings = TRUE))
+  expect_error(dataPrepRast(inputrast, cropWith = mexico, maskWith = mexico,
+                            reprojectTo = inputrast, timings = FALSE))
 
 })
