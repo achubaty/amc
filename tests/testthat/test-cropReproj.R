ba4e5087955044760b293788be46460764685638
Alex Chubaty <alex.chubaty@gmail.com>
2017-04-13 10:59:11 -0600

    always crop *after* reprojecting !!

--- tests/testthat/test-cropReproj.R
+++ tests/testthat/test-cropReproj.R
@@ -20,70 +20,39 @@ test_that("cropReproj works correctly", {
   ext.prj.spdf <- spTransform(ext.spdf, CRS(prj))
 
   ## RasterLayer,RasterLayer
-  sa.rast1 <- crop(r, ext) %>% projectRaster(crs = CRS(prj), method = "ngb")
-  sa.rast2 <- projectRaster(r, crs = CRS(prj), method = "ngb") %>% crop(ext.prj)
+  sa.rast <- projectRaster(r, crs = CRS(prj), method = "ngb") %>% crop(ext.prj)
+  rc1 <- cropReproj(r, sa.rast, layerNames = "test")
 
-  rc1 <- cropReproj(r, sa.rast1, layerNames = "test")
-  rc2 <- cropReproj(r, sa.rast2, layerNames = "test")
-
-  #expect_equal(rc1, rc2)
-  #expect_equivalent(rc1, rc2)
-
-  #expect_equal(sa.rast1, rc1)
-  expect_equivalent(sa.rast1, rc1)
-
-  #expect_equal(sa.rast2, rc2)
-  expect_equivalent(sa.rast2, rc2)
+  expect_equivalent(stack(sa.rast), rc1)
 
   if (interactive()) {
     plot(rc1)
-    plot(rc2)
     plot(ext.prj, add = TRUE)
 
     plot(crop(r, ext))
     plot(ext, add = TRUE)
 
-    plot(sa.rast1)
-    plot(ext.prj, add = TRUE)
-
-    plot(sa.rast2)
+    plot(sa.rast)
     plot(ext.prj, add = TRUE)
   }
 
   ## RasterLayer,SpatialPolygonsDataFrame
-  sa.spdf1 <- crop(r, ext.spdf) %>% projectRaster(crs = CRS(prj), method = "ngb")
-  sa.spdf2 <- projectRaster(r, crs = CRS(prj), method = "ngb") %>% crop(ext.prj.spdf)
+  sa.spdf <- projectRaster(r, crs = CRS(prj), method = "ngb") %>% crop(ext.prj.spdf)
 
-  rc3 <- cropReproj(r, sa.spdf1, layerNames = "test")
-  rc4 <- cropReproj(r, sa.spdf2, layerNames = "test")
+  rc2 <- cropReproj(r, sa.spdf, layerNames = "test")
 
-  #expect_equal(stack(sa.spdf1), rc3)
-  expect_equivalent(stack(sa.spdf1), rc3)
-
-  #expect_equal(stack(sa.spdf2), rc4)
-  expect_equivalent(stack(sa.spdf2), rc4)
+  expect_equivalent(stack(sa.spdf), rc2)
 
   # compare raster vs spdf outputs
-  expect_equivalent(rc1, rc3)
-  expect_equivalent(rc2, rc4)
+  expect_equivalent(rc1, rc2)
 
   ## RasterStack,RasterLayer
   sln <- c("one", "two", "three")
-  sc1 <- cropReproj(s, sa.rast1, layerNames = sln)
-  sc2 <- cropReproj(s, sa.rast2, layerNames = sln)
+  sc1 <- cropReproj(s, sa.rast, layerNames = sln)
 
   # ensure stacks are produced from stacks
   expect_true(is(sc1, "RasterStack"))
-  expect_true(is(sc2, "RasterStack"))
 
-  #expect_equal(sc1, sc2)
-  #expect_equivalent(sc1, sc2)
-
-  sa.stck1 <- stack(sa.rast1, sa.rast1 * 2, sa.rast1 * 3) %>% set_names(sln)
-  #expect_equal(sa.stck1, sc1)
+  sa.stck1 <- stack(sa.rast, sa.rast * 2, sa.rast * 3) %>% set_names(sln)
   expect_equivalent(sa.stck1, sc1)
-
-  sa.stck2 <- stack(sa.rast2, sa.rast2 * 2, sa.rast2 * 3) %>% set_names(sln)
-  #expect_equal(sa.stck2, sc2)
-  expect_equivalent(sa.stck2, sc2)
 })
