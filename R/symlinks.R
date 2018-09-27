edb7247616071a28847de89a03645c875443110a
Alex Chubaty <alex.chubaty@gmail.com>
2018-09-27 12:11:06 -0600

    flink default symlink = TRUE

--- R/symlinks.R
+++ R/symlinks.R
@@ -1,6 +1,6 @@
 #' Create a link to a file
 #'
-#' Creates a hard link to a file if possible, falling back to a symbolic link (symlink).
+#' Creates a symbolic link (symlink) to a file if possible, possibly falling back to a hard link.
 #' Hard links are for files only, and won't work across different physical drives.
 #' Symlinks won't work on Windows without admin privileges.
 #'
@@ -9,7 +9,7 @@
 #' @param from,to  character vectors, containing file names or paths (can alternatively
 #'                 be the path to a single existing directory).
 #' @param symlink  Logical indicating whether to use symlink (instead of hardlink).
-#'                 Default \code{FALSE}.
+#'                 Default \code{TRUE}.
 #'
 #' @seealso \code{\link{file.link}}, \code{\link{file.symlink}}, \code{\link{Sys.readlink}}
 #'
@@ -36,13 +36,13 @@
 #' f2 <- file.path(tmpDir, "file2.csv")
 #'
 #' ## create a link to the the directory; d2 should look like d1
-#' flink(d1, d2)  ## SYMLINK
+#' flink(d1, d2)  ## symlink
 #' dir.exists(d2) ## TRUE
 #' identical(d1, Sys.readlink(d2)) ## TRUE
 #' file.exists(file.path(d2, "file1.csv")) ## TRUE
 #'
 #' ## create link to a file
-#' flink(f0, f2)
+#' flink(f0, f2) ## symlink
 #' file.exists(f2) ## TRUE
 #' identical(f0, Sys.readlink(f2)) ## TRUE
 #'
@@ -73,7 +73,7 @@
 #' ## cleanup
 #' unlink(tmpDir, recursive = TRUE)
 #'
-flink <- function(from, to, symlink = FALSE) {
+flink <- function(from, to, symlink = TRUE) {
   if (isTRUE(symlink)) {
     result <- suppressWarnings(file.symlink(from, to)) ## try symlink
     if (isTRUE(result)) {
@@ -83,7 +83,7 @@ flink <- function(from, to, symlink = FALSE) {
               if (isTRUE(file.exists(to))) paste0("\n  File ", to, " already exists."))
     }
   } else {
-    result <- suppressWarnings(file.link(from, to))  ## try hardlink first
+    result <- suppressWarnings(file.link(from, to))  ## try hardlink
 
     if (isTRUE(result)) {
       message(paste("Created HARDLINK to file", to))
