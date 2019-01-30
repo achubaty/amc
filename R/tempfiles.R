4c2783eb7f6e0f112f64a489e7bc05be02196c32
Alex Chubaty <alex.chubaty@gmail.com>
2019-01-30 12:23:32 -0700

    better tempdir and tempfiles

--- R/tempfiles.R
+++ R/tempfiles.R
@@ -1,18 +1,26 @@
-#' Create a temporary file
+#' Temporary directory and file creation
 #'
-#' This is a wrapper around \code{tempfile()} that actually creates the file,
-#' to ensure a correctly normalized filepath (i.e., on macOS).
+#' These are wrappers around \code{tempdir} and \code{tempfile} that creates the
+#' directory or file, to ensure a correctly normalized filepath (i.e., on macOS).
 #'
+#' @param dir  Path to use as temporary directory. A subdirectory will be created.
+#'             Default is to use the R session temporary directory.
 #' @param ext  File extension to give to the newly create file.
 #'
 #' @return  Character string indicating the filepath to the newly created file.
 #'
 #' @author Alex Chubaty
 #' @export
-#' @rdname tf
-#'
-tf <- function(ext = ".tif") {
-  x <- tempfile(fileext = ext)
+#' @importFrom reproducible checkPath
+#' @rdname tempfiles
+td <- function(dir = tempdir()) {
+  checkPath(dir, create = TRUE)
+}
+
+#' @export
+#' @rdname tempfiles
+tf <- function(ext = ".tif", dir = td()) {
+  x <- tempfile(tmpdir = dir, fileext = ext)
   file.create(x)
   normalizePath(x, winslash = "/", mustWork = NA)
 }
